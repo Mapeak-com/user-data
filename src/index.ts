14509417680cb7b4c6bfc2d7b0e5b9ae344e1aa8
HarelM <harel.mazor@gmail.com>
2026-02-04 13:40:56 +0200

    Fix to allow uploading to imgur and avoid content size restriction.

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,15 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
 app.use(
     middleware({
         apiSpec: apiDocs as any,
-        validateRequests: true,
+        validateRequests: {
+            removeAdditional: 'all'
+        },
         validateResponses: true,
     }),
 );
