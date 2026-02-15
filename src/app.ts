11cc2053884ecb9d8977e5ce908f28a2d939ba08
HarelM <harel.mazor@gmail.com>
2026-02-15 14:59:33 +0200

    Add geolocation to data export

--- src/app.ts
+++ src/app.ts
@@ -355,6 +355,10 @@ app.get('/api/PublicRoutes', async (req: Request, res: Response, next: NextFunct
                     poiLength: url.length,
                     poiDifficulty: url.difficulty,
                     poiId: "Users_" + url.id,
+                    poiGeolocation: {
+                        lon: url.start?.lng,
+                        lat: url.start?.lat
+                    },
                     identifier: url.id,
                     name: url.title,
                     description: url.description,
